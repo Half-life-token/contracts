88a07f064562a4e7ca1891506e631e505ec75dfc
Wezabis <wjorgensen29@gmail.com>
2024-11-17 01:21:37 +0700

    finally

--- contracts/src/HalfLifeToken.sol
+++ contracts/src/HalfLifeToken.sol
@@ -13,6 +13,9 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
     error OVER_MAX_TOKEN_POOL_SIZE();
     error RANDOMNESS_NOT_SENT();
     error ONLY_OWNER();
+    error USERS_CANNOT_PROVIDE_LIQUIDTY();
+    error GAME_IS_NOT_OVER();
+    error NOT_WINNER();
 
     /**********************************************************/
     // Chainlink VRF Variables                                     
@@ -45,9 +48,12 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
     /**********************************************************/
 
     event RandomessFullfilled();
+    event BuyPeriodEnded();
+    event GameEnd();
     event RoundEnded();
+    event RemoveUniswapLiquidity();
 
-    uint256 private constant c_PRICE_PER_TOKEN = 1*(10**5); //$0.1
+    uint256 private constant c_PRICE_PER_TOKEN = 1*(10**4); //$0.01
     uint256 private constant c_TOTAL_TOKEN_POOL = 100000; //100,000
     uint256 private constant c_BUY_PERIOD = 1 days;
 
@@ -63,6 +69,7 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
     address private s_uniswapV3Pool;
     mapping(address => uint256) private s_holderIndices;
     mapping(address => uint256) private s_liquidityProvider;
+    bool private s_gameOver = false;
 
 
 
@@ -93,7 +100,7 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
         s_uniswapV3Pool = _uniswapPool;
     }
 
-    function buy(uint256 _tokensToBuy) external payable GameNotStarted {
+    function buy(uint256 _tokensToBuy) external GameNotStarted {
         if(i_USDC.allowance(msg.sender, address(this)) < (c_PRICE_PER_TOKEN * _tokensToBuy)){
             revert NOT_ENOUGH_ALLOWANCE();
         }
@@ -119,7 +126,7 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
         }
         
         if(s_totalTokenPoolRemaining == 0 || s_holders.length > 100 || i_blockStart + c_BUY_PERIOD < block.number) {
-            emit RoundEnded();
+            emit BuyPeriodEnded();
         }
 
 
@@ -129,51 +136,124 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
         address owner = _msgSender();
         _transfer(owner, to, value);
 
-        if(to != s_uniswapV3Pool){
-            if(balanceOf(owner) == 0){
-                s_isHolder[owner] = false;
-                uint256 indexToRemove = s_holderIndices[owner];
-                s_holders[indexToRemove] = s_holders[s_holders.length - 1];
-                s_holderIndices[s_holders[indexToRemove]] = indexToRemove;
-                s_holders.pop();
-                delete s_holderIndices[owner];
-            }
+        if(balanceOf(owner) == 0){
+            s_isHolder[owner] = false;
+            uint256 indexToRemove = s_holderIndices[owner];
+            s_holders[indexToRemove] = s_holders[s_holders.length - 1];
+            s_holderIndices[s_holders[indexToRemove]] = indexToRemove;
+            s_holders.pop();
+            delete s_holderIndices[owner];
         }
 
         if(to == s_uniswapV3Pool){
-            s_liquidityProvider[msg.sender] = value;
+            revert USERS_CANNOT_PROVIDE_LIQUIDTY();
         }
 
         return true;
     }
 
-    function halfing() external onlyServer {
+    function half() external onlyServer {
         if(!requestSent){
             revert RANDOMNESS_NOT_SENT();
         }
         
         uint256 numberToHalf = totalSupply() / 2;
-
-        if(totalSupply() == 3){
-            numberToHalf = 4;
+        uint256 randomValue = s_requests[requestIds[requestIds.length - 1]].randomWords[0];
+        
+        if(totalSupply() <= 3){
+            if(totalSupply() == 2 || totalSupply() == 3) {
+                _handleEndgame(randomValue);
+            } else {
+                _handleNormalHalving(numberToHalf, randomValue);
+            }
         }
 
-        uint256 addressIterator = 0;
-        for(uint x; x < numberToHalf; x++){
+        emit RoundEnded();
+    }
 
+    function _handleEndgame(uint256 randomValue) internal {
+        if(totalSupply() == 3) {
+            _burnRandomHolders(randomValue, 2);
+            s_gameOver = true;
+            emit GameEnd();
+        } else if(totalSupply() == 2) {
+            _burnRandomHolders(randomValue, 1);
+            s_gameOver = true;
+            emit GameEnd();
+        }
+    }
+
+    function _handleNormalHalving(uint256 numberToHalf, uint256 randomValue) internal {
+        bytes memory randomBytes = abi.encodePacked(randomValue);
+        uint8 lastByte = uint8(randomBytes[randomBytes.length - 1]) &1;
+        uint256 addressIterator = 0;
+
+        for(uint x; x < numberToHalf; x++){
             if(addressIterator == s_holders.length){
+                if(numberToHalf - x < s_holders.length){
+                    _burnRandomHolders(randomValue, numberToHalf - x);
+                    break;
+                }
                 addressIterator = 0;
             }
 
             address participant = s_holders[addressIterator];
+            if(_shouldBurnToken(participant, randomValue, lastByte)) {
+                _burnParticipantToken(participant);
+            }
+            addressIterator++;
+        }
+    }
 
-            
+    function _shouldBurnToken(address participant, uint256 randomValue, uint8 lastByte) internal pure returns (bool) {
+        bytes32 hash = keccak256(abi.encodePacked(participant, randomValue));
+        uint8 lastByteHash = uint8(hash[hash.length - 1]) &1;
+        return lastByteHash == lastByte;
+    }
+
+    function _burnParticipantToken(address participant) internal {
+        if(participant == s_uniswapV3Pool) {
+            emit RemoveUniswapLiquidity();
+        } else {
+            _burn(participant, 1);
         }
 
-        if(numberToHalf == 2) {
-            
+        if(balanceOf(participant) < 1){
+            _removeHolder(participant);
+        }
+    }
+
+    function _burnRandomHolders(uint256 randomValue, uint256 count) internal {
+        for(uint i = 0; i < count; i++) {
+            uint256 index = randomValue % s_holders.length;
+            _burnParticipantToken(s_holders[index]);
+            randomValue = uint256(keccak256(abi.encode(randomValue)));
+        }
+    }
+
+    function _removeHolder(address holder) internal {
+        uint256 indexToRemove = s_holderIndices[holder];
+        s_holders[indexToRemove] = s_holders[s_holders.length - 1];
+        s_holderIndices[s_holders[indexToRemove]] = indexToRemove;
+        s_holders.pop();
+        delete s_holderIndices[holder];
+        s_isHolder[holder] = false;
+    }
+
+    function cashOut() external {
+        if(!s_gameOver){
+            revert GAME_IS_NOT_OVER();
         }
 
+        if(balanceOf(msg.sender) < 1){
+            revert NOT_WINNER();
+        }
+
+        bool success = i_USDC.transfer(msg.sender, i_USDC.balanceOf(address(this)));
+
+        if(!success){
+            revert TRANSFER_FAILED();
+        }
     }
 
 
@@ -248,6 +328,7 @@ contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
     ) internal override {
         require(s_requests[_requestId].exists, "request not found");
         s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
 
         emit RandomessFullfilled();
     }
