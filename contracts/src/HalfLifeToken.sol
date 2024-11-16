b09c1449177632bfeb5f39f425bc21ff026d343a
Wezabis <wjorgensen29@gmail.com>
2024-11-16 20:20:16 +0700

    update

--- contracts/src/HalfLifeToken.sol
+++ contracts/src/HalfLifeToken.sol
@@ -2,23 +2,77 @@
 pragma solidity 0.8.20;
 
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
-import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {VRFConsumerBaseV2Plus} from "chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract HalfLife is ERC20, Ownable {
+contract HalfLife is ERC20, VRFConsumerBaseV2Plus {
 
     error NOT_ENOUGH_ALLOWANCE();
     error TRANSFER_FAILED();
     error GAME_STARTED();
     error OVER_MAX_TOKEN_POOL_SIZE();
+    error RANDOMNESS_NOT_SENT();
+    error ONLY_OWNER();
+
+    /**********************************************************/
+    // Chainlink VRF Variables                                     
+    /**********************************************************/
+    event RequestSent(uint256 requestId, address sender);
+
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists; 
+        uint256[] randomWords;
+    }
+
+    mapping(uint256 => RequestStatus) public s_requests;
+    bool private requestSent = false;
+
+    uint256 public s_subscriptionId = 25608016536000951774293845819229934175992854276553215023064642835696496121759;
+
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // Current base
+    bytes32 public keyHash = 0xdc2f87677b01473c763cb0aee938ed3341512f6057324a584e5944e786144d70;
+    uint32 public callbackGasLimit = 100000;
+    uint16 public requestConfirmations = 3;
+    uint32 public numWords = 1;
+    address private vrfCoordinator = 0xd5D517aBE5cF79B7e95eC98dB0f0277788aFF634;
+
+    /**********************************************************/
+    // Half Life Variables                                     
+    /**********************************************************/
+
+    event RandomessFullfilled();
+    event RoundEnded();
 
     uint256 private constant c_PRICE_PER_TOKEN = 1*(10**5); //$0.1
     uint256 private constant c_TOTAL_TOKEN_POOL = 100000; //100,000
+    uint256 private constant c_BUY_PERIOD = 1 days;
 
     ERC20 immutable i_USDC;
+    uint256 immutable i_blockStart;
+    address immutable i_owner;
 
-    uint256 s_lotteryPool;
-    uint256 s_totalTokenPoolRemaining = 100000;
-    bool gameStarted = false;
+    uint256 private s_lotteryPool;
+    uint256 private s_totalTokenPoolRemaining = 100000;
+    bool private gameStarted = false;
+    address[] private s_holders;
+    mapping(address => bool) private s_isHolder;
+    address private s_uniswapV3Pool;
+    mapping(address => uint256) private s_holderIndices;
+    mapping(address => uint256) private s_liquidityProvider;
+
+
+
+    modifier onlyServer() {
+        if(msg.sender != i_owner){
+            revert ONLY_OWNER();
+        }
+        _;
+    }
+
 
     modifier GameNotStarted() {
         if(gameStarted) {
@@ -27,10 +81,16 @@ contract HalfLife is ERC20, Ownable {
         _;
     }
 
-    constructor(address _usdc) ERC20("HalfLife", "HLF") Ownable(msg.sender) {
+    constructor(address _usdc) ERC20("HalfLife", "HLF") VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_USDC = ERC20(_usdc);
+        i_blockStart = block.number;
+        i_owner = msg.sender;
         //mint to wallet for liquidity add
-        // subtract from total token pool 
+        //subtract from total token pool 
+    }
+
+    function setPoolAddress(address _uniswapPool) external onlyServer {
+        s_uniswapV3Pool = _uniswapPool;
     }
 
     function buy(uint256 _tokensToBuy) external payable GameNotStarted {
@@ -49,15 +109,147 @@ contract HalfLife is ERC20, Ownable {
         }
 
         _mint(msg.sender, _tokensToBuy);
+
+        s_totalTokenPoolRemaining -= _tokensToBuy;
+
+        if(!s_isHolder[msg.sender]){
+            s_isHolder[msg.sender] = true;
+            s_holderIndices[msg.sender] = s_holders.length;
+            s_holders.push(msg.sender);
+        }
+        
+        if(s_totalTokenPoolRemaining == 0 || s_holders.length > 100 || i_blockStart + c_BUY_PERIOD < block.number) {
+            emit RoundEnded();
+        }
+
+
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+
+        if(to != s_uniswapV3Pool){
+            if(balanceOf(owner) == 0){
+                s_isHolder[owner] = false;
+                uint256 indexToRemove = s_holderIndices[owner];
+                s_holders[indexToRemove] = s_holders[s_holders.length - 1];
+                s_holderIndices[s_holders[indexToRemove]] = indexToRemove;
+                s_holders.pop();
+                delete s_holderIndices[owner];
+            }
+        }
+
+        if(to == s_uniswapV3Pool){
+            s_liquidityProvider[msg.sender] = value;
+        }
+
+        return true;
+    }
+
+    function halfing() external onlyServer {
+        if(!requestSent){
+            revert RANDOMNESS_NOT_SENT();
+        }
+        
+        uint256 numberToHalf = totalSupply() / 2;
+
+        if(totalSupply() == 3){
+            numberToHalf = 4;
+        }
+
+        uint256 addressIterator = 0;
+        for(uint x; x < numberToHalf; x++){
+
+            if(addressIterator == s_holders.length){
+                addressIterator = 0;
+            }
+
+            address participant = s_holders[addressIterator];
+
+            
+        }
+
+        if(numberToHalf == 2) {
+            
+        }
+
     }
 
 
 
-    //Owner Functions
-    function startGame() external onlyOwner GameNotStarted {
-        gameStarted = true;
+    /**********************************************************/
+    // Chainlink VRF Functions                                    
+    /**********************************************************/
+
+
+    function requestRandomWords() internal {
+        uint256 requestId;
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: true
+                    })
+                )
+            })
+        );
+
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        requestSent = true;
+        emit RequestSent(requestId, msg.sender);
+    }
+
+    function requestRandomWordsForServer() external onlyServer {
+        uint256 requestId;
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: true
+                    })
+                )
+            })
+        );
+
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        requestSent = true;
+        emit RequestSent(requestId, msg.sender);
     }
 
 
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+
+        emit RandomessFullfilled();
+    }
 
 }
